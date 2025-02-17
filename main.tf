25a0bc119478689eb85972a11ba620562e1fd6a3
sunil-challagali <108851025+sunil-challagali@users.noreply.github.com>
2025-02-17 18:13:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "ap-south-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-xxxxxxxx" # Replace with your built AMI ID
+resource "aws_instance" "packer" {
+  ami           = "ami-00bb6a80f01f03502" # Replace with your built AMI ID
   instance_type = "t2.micro"
 }
 
 output "instance_id" {
-  value = aws_instance.example.id
+  value = aws_instance.packer.id
 }
