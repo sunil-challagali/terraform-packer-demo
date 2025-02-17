cddd2407ac4979a268c49ccd69f805bf0d33cd8c
sunil-challagali <108851025+sunil-challagali@users.noreply.github.com>
2025-02-17 18:42:42 +0530

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-apt update
-apt install -y nginx
+sudo apt update
+sudo apt install -y nginx
 sudo sed -i 's/<h1>Welcome to nginx!<\/h1>/<h1>Welcome to Terraform with Packer<\/h1>/' /var/www/html/index.nginx-debian.html
 sudo systemctl start nginx
 sudo systemctl enable nginx
