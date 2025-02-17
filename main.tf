f8b3b9f4183be2eabf7121961f1ffab29a73c858
sunil-challagali <108851025+sunil-challagali@users.noreply.github.com>
2025-02-17 18:25:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,21 @@
 provider "aws" {
   region = "ap-south-1"
 }
-terraform {
-  backend "s3" {
-    bucket = "my-terraform-state-bucket-sunil7756"
-    key    = "terraform-packer/terraform.tfstate"
-    region = "ap-south-1"
-  }
+
+# Replace this with the AMI ID that Packer generated (you can get it from the output or the Packer manifest file)
+variable "ami_id" {
+  description = "The AMI ID of the custom AMI created by Packer"
+  type        = string
 }
+
 resource "aws_instance" "packer" {
-  ami           = "ami-00bb6a80f01f03502" # Replace with your built AMI ID
+  ami           = var.ami_id  # Use the AMI ID from the variable
   instance_type = "t2.micro"
+  key_name      = "dockernew"  # Replace with your SSH key name
+
+  tags = {
+    Name = "MyInstance"
+  }
 }
 
 output "instance_id" {
