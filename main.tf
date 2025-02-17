76dbb2513b5331837d3ddf8356467f8e9317ddf9
sunil-challagali <108851025+sunil-challagali@users.noreply.github.com>
2025-02-17 18:19:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,13 @@
 provider "aws" {
   region = "ap-south-1"
 }
-
+terraform {
+  backend "s3" {
+    bucket = "my-terraform-state-bucket-sunil7756"
+    key    = "terraform-packer/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
 resource "aws_instance" "packer" {
   ami           = "ami-00bb6a80f01f03502" # Replace with your built AMI ID
   instance_type = "t2.micro"
